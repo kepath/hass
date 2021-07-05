b72c49ca569f75e480ea0c2e8ed987a73c4c8e3c
Kevin Thorn <kevin@kevinthorn.com>
2021-07-05 20:41:56 +0100

    HACS

--- custom_components/powercalc/__init__.py
+++ custom_components/powercalc/__init__.py
@@ -2,38 +2,36 @@
 
 from __future__ import annotations
 
-from .const import (
-    CONF_MODE,
-    CONF_MIN_WATT,
-    CONF_MAX_WATT,
-    CONF_WATT,
-    DOMAIN,
-    DATA_CALCULATOR_FACTORY,
-    MODE_FIXED,
-    MODE_LINEAR,
-    MODE_LUT
-)
+import logging
+from typing import Optional
 
 from homeassistant.helpers.typing import HomeAssistantType
 
-from .errors import StrategyConfigurationError
-
-from .strategy_lut import (
-    LutRegistry,
-    LutStrategy
+from .const import (
+    CONF_FIXED,
+    CONF_LINEAR,
+    CONF_MAX_POWER,
+    CONF_MAX_WATT,
+    CONF_MIN_POWER,
+    CONF_MIN_WATT,
+    CONF_POWER,
+    CONF_STATES_POWER,
+    CONF_WATT,
+    DATA_CALCULATOR_FACTORY,
+    DOMAIN,
+    MODE_FIXED,
+    MODE_LINEAR,
+    MODE_LUT,
 )
-from .strategy_linear import (
-    LinearStrategy
-)
-from .strategy_fixed import (
-    FixedStrategy
-)
-from.strategy_interface import (
-    PowerCalculationStrategyInterface
-)
-
+from .errors import StrategyConfigurationError, UnsupportedMode
 from .light_model import LightModel
-from .errors import UnsupportedMode
+from .strategy_fixed import FixedStrategy
+from .strategy_interface import PowerCalculationStrategyInterface
+from .strategy_linear import LinearStrategy
+from .strategy_lut import LutRegistry, LutStrategy
+
+_LOGGER = logging.getLogger(__name__)
+
 
 async def async_setup(hass: HomeAssistantType, config: dict) -> bool:
     hass.data.setdefault(DOMAIN, {})
@@ -41,46 +39,75 @@ async def async_setup(hass: HomeAssistantType, config: dict) -> bool:
 
     return True
 
+
 class PowerCalculatorStrategyFactory:
     def __init__(self, hass: HomeAssistantType) -> None:
         self._hass = hass
         self._lut_registry = LutRegistry()
 
-    def create(self, config: dict, mode: str, light_model: LightModel) -> PowerCalculationStrategyInterface:
+    def create(
+        self,
+        config: dict,
+        mode: str,
+        light_model: Optional[LightModel],
+        entity_domain: str,
+    ) -> PowerCalculationStrategyInterface:
         """Create instance of calculation strategy based on configuration"""
-        if (mode == MODE_LINEAR):
-            return self.create_linear(config, light_model)
-        
-        if (mode == MODE_FIXED):
-            return self.create_fixed(config, light_model)
-        
-        if (mode == MODE_LUT):
-            return self.create_lut(light_model)
-        
+        if mode == MODE_LINEAR:
+            return self._create_linear(config, light_model, entity_domain)
+
+        if mode == MODE_FIXED:
+            return self._create_fixed(config, light_model)
+
+        if mode == MODE_LUT:
+            return self._create_lut(light_model)
+
         raise UnsupportedMode("Invalid calculation mode", mode)
-    
-    def create_linear(self, config: dict, light_model: LightModel) -> LinearStrategy:
+
+    def _create_linear(
+        self, config: dict, light_model: LightModel, entity_domain: str
+    ) -> LinearStrategy:
         """Create the linear strategy"""
-        min = config.get(CONF_MIN_WATT)
-        max = config.get(CONF_MAX_WATT)
-        if (min is None and max is None):
-            min = light_model.linear_mode_config.get(CONF_MIN_WATT)
-            max = light_model.linear_mode_config.get(CONF_MAX_WATT)
+        linear_config = config.get(CONF_LINEAR)
 
-        return LinearStrategy(min, max)
-    
-    def create_fixed(self, config: dict, light_model: LightModel) -> FixedStrategy:
+        if linear_config is None:
+            # Below is for BC compatibility
+            if config.get(CONF_MIN_WATT) is not None:
+                _LOGGER.warning(
+                    "min_watt is deprecated and will be removed in version 0.3, use linear->min_power"
+                )
+                linear_config = {
+                    CONF_MIN_POWER: config.get(CONF_MIN_WATT),
+                    CONF_MAX_POWER: config.get(CONF_MAX_WATT),
+                }
+
+            elif light_model is not None:
+                linear_config = light_model.linear_mode_config
+
+        return LinearStrategy(linear_config, entity_domain)
+
+    def _create_fixed(self, config: dict, light_model: LightModel) -> FixedStrategy:
         """Create the fixed strategy"""
-        return FixedStrategy(
-            config.get(CONF_WATT) or light_model.fixed_mode_config.get(CONF_WATT)
-        )
-    
-    def create_lut(self, light_model: LightModel) -> LutStrategy:
-        """Create the lut strategy"""
-        if (light_model is None):
-            raise StrategyConfigurationError("You must supply a valid manufacturer and model to use the LUT mode")
+        fixed_config = config.get(CONF_FIXED)
+        if fixed_config is None and light_model is not None:
+            fixed_config = light_model.fixed_mode_config
 
-        return LutStrategy(
-            self._lut_registry,
-            light_model
+        # BC compat
+        if fixed_config is None:
+            _LOGGER.warning(
+                "watt is deprecated and will be removed in version 0.3, use fixed->power"
+            )
+            fixed_config = {CONF_POWER: config.get(CONF_WATT)}
+
+        return FixedStrategy(
+            fixed_config.get(CONF_POWER), fixed_config.get(CONF_STATES_POWER)
         )
+
+    def _create_lut(self, light_model: LightModel) -> LutStrategy:
+        """Create the lut strategy"""
+        if light_model is None:
+            raise StrategyConfigurationError(
+                "You must supply a valid manufacturer and model to use the LUT mode"
+            )
+
+        return LutStrategy(self._lut_registry, light_model)
--- custom_components/powercalc/strategy_interface.py
+++ custom_components/powercalc/strategy_interface.py
@@ -1,9 +1,11 @@
-from homeassistant.core import State
 from typing import Optional
+
 import homeassistant.helpers.entity_registry as er
+from homeassistant.core import State
+
 
 class PowerCalculationStrategyInterface:
-    async def calculate(self, light_state: State) -> Optional[int]:
+    async def calculate(self, entity_state: State) -> Optional[int]:
         """Calculate power consumption based on entity state"""
         pass
 
