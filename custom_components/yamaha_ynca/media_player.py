26c043e737851eafdfc68773b5bb5bfcc7109698
Kevin Thorn <kevin@kevinthorn.com>
2022-09-09 21:30:10 +0100

    hacs

--- custom_components/yamaha_ynca/media_player.py
+++ custom_components/yamaha_ynca/media_player.py
@@ -25,6 +25,7 @@ from homeassistant.const import (
 
 
 from .const import (
+    CONF_HIDDEN_SOUND_MODES,
     DOMAIN,
     LOGGER,
     ZONE_MIN_VOLUME,
@@ -70,12 +71,15 @@ async def async_setup_entry(hass, config_entry: ConfigEntry, async_add_entities)
             hidden_inputs = config_entry.options.get(
                 CONF_HIDDEN_INPUTS_FOR_ZONE(zone_subunit_id), []
             )
+            hidden_sound_modes = config_entry.options.get(CONF_HIDDEN_SOUND_MODES, [])
+
             entities.append(
                 YamahaYncaZone(
                     config_entry.entry_id,
                     domain_entry_data.api,
                     zone_subunit,
                     hidden_inputs,
+                    hidden_sound_modes,
                 )
             )
 
@@ -95,10 +99,12 @@ class YamahaYncaZone(MediaPlayerEntity):
         ynca: ynca.Ynca,
         zone: Type[ynca.zone.ZoneBase],
         hidden_inputs: List[str],
+        hidden_sound_modes: List[str],
     ):
         self._ynca = ynca
         self._zone = zone
         self._hidden_inputs = hidden_inputs
+        self._hidden_sound_modes = hidden_sound_modes
 
         self._attr_unique_id = f"{receiver_unique_id}_{self._zone.id}"
         self._attr_device_info = {
@@ -221,7 +227,13 @@ class YamahaYncaZone(MediaPlayerEntity):
             sound_modes.append(STRAIGHT)
         if self._zone.soundprg:
             modelinfo = ynca.get_modelinfo(self._ynca.SYS.modelname)
-            sound_modes.extend(modelinfo.soundprg if modelinfo else ynca.SoundPrg)
+            filtered_sound_modes = [
+                sound_mode.value
+                for sound_mode in (modelinfo.soundprg if modelinfo else ynca.SoundPrg)
+                if sound_mode.name not in self._hidden_sound_modes
+            ]
+            sound_modes.extend(filtered_sound_modes)
+
         sound_modes.sort(
             key=str.lower
         )  # Using `str.lower` does not work for all languages, but better than nothing
@@ -242,9 +254,9 @@ class YamahaYncaZone(MediaPlayerEntity):
                     supported_commands |= MediaPlayerEntityFeature.PAUSE
                     supported_commands |= MediaPlayerEntityFeature.NEXT_TRACK
                     supported_commands |= MediaPlayerEntityFeature.PREVIOUS_TRACK
-            if hasattr(input_subunit, "repeat"):
+            if getattr(input_subunit, "repeat", None) is not None:
                 supported_commands |= MediaPlayerEntityFeature.REPEAT_SET
-            if hasattr(input_subunit, "shuffle"):
+            if getattr(input_subunit, "shuffle", None) is not None:
                 supported_commands |= MediaPlayerEntityFeature.SHUFFLE_SET
         return supported_commands
 
@@ -305,8 +317,7 @@ class YamahaYncaZone(MediaPlayerEntity):
     def shuffle(self) -> Optional[bool]:
         """Boolean if shuffle is enabled."""
         if subunit := self._input_subunit():
-            if hasattr(subunit, "shuffle"):
-                return subunit.shuffle
+            return getattr(subunit, "shuffle", None)
         return None
 
     def set_shuffle(self, shuffle):
@@ -317,12 +328,12 @@ class YamahaYncaZone(MediaPlayerEntity):
     def repeat(self) -> Optional[str]:
         """Return current repeat mode."""
         if subunit := self._input_subunit():
-            if hasattr(subunit, "repeat"):
-                if subunit.repeat == ynca.Repeat.SINGLE:
+            if repeat := getattr(subunit, "repeat", None):
+                if repeat == ynca.Repeat.SINGLE:
                     return REPEAT_MODE_ONE
-                if subunit.repeat == ynca.Repeat.ALL:
+                if repeat == ynca.Repeat.ALL:
                     return REPEAT_MODE_ALL
-                if subunit.repeat == ynca.Repeat.OFF:
+                if repeat == ynca.Repeat.OFF:
                     return REPEAT_MODE_OFF
         return None
 
@@ -343,7 +354,7 @@ class YamahaYncaZone(MediaPlayerEntity):
         if subunit := self._input_subunit():
             if subunit.id in RADIO_SOURCES:
                 return MEDIA_TYPE_CHANNEL
-            if hasattr(subunit, "song"):
+            if getattr(subunit, "song", None) is not None:
                 return MEDIA_TYPE_MUSIC
         return None
 
