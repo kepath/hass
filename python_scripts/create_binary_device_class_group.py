e0c480772c6919436801101f17efc2db1b893efb
Kevin Thorn <kevin@kevinthorn.com>
2023-01-19 19:52:28 +0000

    minimalist changes

--- python_scripts/create_binary_device_class_group.py
+++ python_scripts/create_binary_device_class_group.py
@@ -9,23 +9,33 @@ entity_list = []
 if not isinstance(group, str) or not domain or not group:
     logger.warning("bad domain or group! Not executing.")
 
-name = "All binary sensor {} group".format(collection_name)
+name = f"All binary sensor {collection_name} group"
+excluded_entities = [
+    "binary_sensor.zigbee_door_sensor_bed_c_contact",
+    "light.chrome_workdell_screen",
+    "light.c105a8ea_57917284_screen",
+    "binary_sensor.chrome_workdell",
+    "binary_sensor.c105a8ea_57917284",
+    "binary_sensor.front_g3_flex_motion",
+    "binary_sensor.diskstation_security_status"
+]
 
 for entity_id in hass.states.entity_ids(domain):
     if entity_id == "":
-        logger.error(logger, "**Required parameter 'entity_id' is missing.**\n\nAction '{}' NOT executed.".format(action.lower()))
+        logger.error(logger, f"**Required parameter 'entity_id' is missing.**\n\nAction '{action.lower()}' NOT executed.")
     else:
-        entity = hass.states.get(entity_id)
-        if entity is None:
-            logger.error(logger, "**Cannot find entity '{}'.**\n\nAction '{}' NOT executed.".format(entity_id, action.lower()))
-        else:
-            for attr in entity.attributes:
-                if attr is not None:
-                    if attr == "device_class":
-                        for device_class_option in included_device_classes:
-                            if entity.attributes.get(attr) == device_class_option:
-                                entity_list.append(entity_id)
-                                logger.info("A {} at {}".format(entity_id, time.time()))
+        if entity_id not in excluded_entities:
+            entity = hass.states.get(entity_id)
+            if entity is None:
+                logger.error(logger, f"**Cannot find entity '{entity_id}'.**\n\nAction '{action.lower()}' NOT executed.")
+            else:
+                for attr in entity.attributes:
+                    if attr is not None:
+                        if attr == "device_class":
+                            for device_class_option in included_device_classes:
+                                if entity.attributes.get(attr) == device_class_option:
+                                    entity_list.append(entity_id)
+                                    logger.info(f"A {entity_id} at {time.time()}")
 
 if not icon:
     service_data = {"object_id": group, "entities": entity_list, "name": name}
