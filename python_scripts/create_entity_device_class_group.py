eee766b59d6730a93218b04475f7befb027378f4
Kevin Thorn <kevin@kevinthorn.com>
2023-05-21 22:28:29 +0100

    group create python script maintenance

--- python_scripts/create_entity_device_class_group.py
+++ python_scripts/create_entity_device_class_group.py
@@ -5,11 +5,14 @@
 # group_name - the name of the group to create or recreate
 # icon - the mdi icon to assign to this group after creation
 # friendly_name - the friendly name of the group
+# filter_by_device_class - a boolean to indicate whether to filter by device class
+#   if set to false, all entities in the domain will be added to the group
 # included_device_classes - a list of device classes to include in the group
 #   examples of device classes can be found here
 #   binary_sensor - https://developers.home-assistant.io/docs/core/entity/binary-sensor
 #   switch - https://developers.home-assistant.io/docs/core/entity/switch
-# excluded_entities - a list of entity_id's to exclude from the group
+# excluded_entities - a list of entity_id's to exclude from the group.
+#   Be aware that hyphens are replaced by underscores in the entity_id's
 #
 # An example of an automation being used to create this group is
 #
@@ -40,6 +43,7 @@ domain = data.get('domain', '')
 group_name = data.get('group_name', '')
 icon = data.get('icon', '')
 friendly_name = data.get('friendly_name', group_name)
+filter_by_device_class = data.get('filter_by_device_class', False)
 included_device_classes = data.get('included_device_classes', ['door'])
 excluded_entities = data.get('excluded_entities', [])
 
@@ -61,8 +65,21 @@ globally_excluded_matches = [
     "macbook_pro_2018_chrome",
     "a996ec86_cdbe8ea4",
     "kev_iphone_remote",
-    "41edef0a-ffdd99dd"
+    "41edef0a_ffdd99dd"
 ]
+
+# try:
+logger.info(f"Entering integration_entities")
+# for entity_id in hass.integration_entities('browser_mod'):
+#     logger.error(logger, f"a: Adding '{entity_id}' to the globally excluded matches list")
+# for entity_id in hass.helpers.template.integration_entities('browser_mod'):
+#     logger.error(logger, f"b: Adding '{entity_id}' to the globally excluded matches list")
+# for entity_id in hass.entity_registry.integration_entities('browser_mod'):
+#     logger.error(logger, f"c: Adding '{entity_id}' to the globally excluded matches list")
+# for entity_id in hass.entry.integration_entities('browser_mod'):
+#     logger.error(logger, f"d: Adding '{entity_id}' to the globally excluded matches list")
+# except:
+#     logger.error(logger, f"Error - a problem occured adding browser_mod entities to the globally excluded matches list")
 
 try:
     for entity_id in hass.states.entity_ids(domain):
@@ -74,13 +91,18 @@ try:
                 if entity is None:
                     logger.error(logger, f"Error - entity object not found")
                 else:
-                    for attr in entity.attributes:
-                        if attr is not None:
-                            if attr == "device_class":
-                                for device_class_option in included_device_classes:
-                                    if entity.attributes.get(attr) == device_class_option:
-                                        entity_list.append(entity_id)
-                                        logger.debug(f"'{entity_id}' added to group '{friendly_name}' at {time.time()}")
+                    if filter_by_device_class:
+                        for attr in entity.attributes:
+                            if attr is not None:
+                                if attr == "device_class":
+                                    for device_class_option in included_device_classes:
+                                        if entity.attributes.get(attr) == device_class_option:
+                                            entity_list.append(entity_id)
+                                            logger.debug(f"'{entity_id}' added to group '{friendly_name}' at {time.time()}")
+                    else:
+                        entity_list.append(entity_id)
+                        logger.debug(f"'{entity_id}' added to group '{friendly_name}' at {time.time()}")
+
 except:
     logger.error(logger, f"Error - a problem occured creating the entity list")
 
@@ -89,7 +111,7 @@ try:
         for match_entity in entity_list:
             if match_entity.find(excluded_matches) != -1:
                 entity_list.remove(match_entity)
-                logger.debug(f"'{match_entity}' removed from group '{friendly_name}' as it matched '{match_entity}' at {time.time()}")
+                logger.debug(f"'{match_entity}' removed from group '{friendly_name}' as it matched '{excluded_matches}' at {time.time()}")
 
 except:
     logger.error(logger, f"Error - a problem occured when removing a matched item from the entity list")
