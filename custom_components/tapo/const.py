e2c6d9e7d2cbab8e14b1c83b26fcd9d8e2e4cbc1
Kevin Thorn <kevin@kevinthorn.com>
2023-06-21 22:00:55 +0100

    hacs

--- custom_components/tapo/const.py
+++ custom_components/tapo/const.py
@@ -5,9 +5,9 @@ from plugp100 import LightEffectPreset
 
 NAME = "tapo"
 DOMAIN = "tapo"
-VERSION = "1.6.1"
+VERSION = "1.7.1"
 
-SUPPORTED_DEVICE_AS_SWITCH = ["p100", "p105", "p110", "p115", "p125"]
+SUPPORTED_DEVICE_AS_SWITCH = ["p100", "p105", "p110", "p115", "p125", "p125m"]
 SUPPORTED_DEVICE_AS_SWITCH_POWER_MONITOR = ["p110", "p115"]
 SUPPORTED_DEVICE_AS_LIGHT = {
     "l920": [ColorMode.ONOFF, ColorMode.BRIGHTNESS, ColorMode.HS],
@@ -88,6 +88,12 @@ PLATFORMS = ["switch", "light", "sensor", "binary_sensor"]
 CONF_HOST = "host"
 CONF_USERNAME = "username"
 CONF_PASSWORD = "password"
+CONF_ADVANCED_SETTINGS = "advanced_settings"
+
+STEP_INIT = "user"
+STEP_ADVANCED_SETTINGS = "advanced_config"
+
+DEFAULT_POLLING_RATE_S = 30  # 30 seconds
 
 CONF_ALTERNATIVE_IP = "ip_address"
 
