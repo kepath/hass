1144e8decab93b36c7c6d7156986e8a37fbcb9bc
Kevin Thorn <github@kevinthorn.com>
2024-09-19 16:52:28 +0100

    hacs

---
+++ custom_components/solis_modbus/modbus_controller.py
@@ -0,0 +1,77 @@
+import asyncio
+import logging
+
+from pymodbus.client import AsyncModbusTcpClient
+
+_LOGGER = logging.getLogger(__name__)
+
+
+class ModbusController:
+    def __init__(self, host, port=502):
+        self.host = host
+        self.port = port
+        self.client: AsyncModbusTcpClient = AsyncModbusTcpClient(self.host, port=self.port)
+        self.connect_failures = 0
+        self._lock = asyncio.Lock()
+
+    async def connect(self):
+        _LOGGER.debug('connecting')
+        try:
+            if self.client.connected:
+                return True
+
+            if not await self.client.connect():
+                self.connect_failures += 1
+                raise _LOGGER.warning(
+                    f"Failed to connect to Modbus device. Will retry, failures = {self.connect_failures}")
+            else:
+                self.connect_failures = 0
+            return True
+
+        except Exception as e:
+            raise _LOGGER.error(f"Failed to connect to Modbus device. Will retry")
+
+    async def async_read_input_register(self, register, count=1):
+        try:
+            await self.connect()
+            async with self._lock:
+                result = await self.client.read_input_registers(register, count, slave=1)
+                _LOGGER.debug(f'register value, register = {register}, result = {result.registers}')
+            return result.registers
+        except Exception as e:
+            raise _LOGGER.error(f"Failed to read Modbus register: {str(e)}")
+
+    async def async_read_holding_register(self, register: int, count=1):
+        try:
+            await self.connect()
+            async with self._lock:
+                result = await self.client.read_holding_registers(register, count, slave=1)
+                _LOGGER.debug(f'holding register value, register = {register}, result = {result.registers}')
+            return result.registers
+        except Exception as e:
+            raise _LOGGER.error(f"Failed to read Modbus holding register: {str(e)}")
+
+    async def async_write_holding_register(self, register: int, value):
+        try:
+            await self.connect()
+            async with self._lock:
+                result = await self.client.write_register(register, value, slave=1)
+            return result
+        except Exception as e:
+            raise _LOGGER.error(f"Failed to write Modbus holding register ({register}): {str(e)}")
+
+    async def async_write_holding_registers(self, start_register: int, values: list[int]):
+        try:
+            await self.connect()
+            async with self._lock:
+                result = await self.client.write_registers(start_register, values, slave=1)
+            return result
+        except Exception as e:
+            raise _LOGGER.error(
+                f"Failed to write Modbus holding registers ({start_register}), values = {values}: {str(e)}")
+
+    def close_connection(self):
+        self.client.close()
+
+    def connected(self):
+        return self.client.connected
