f12191ed77e518e5dafd95aa5640eaded229c933
Kevin Thorn <github@kevinthorn.com>
2025-01-14 09:47:53 +0000

    remove third-party python scripts. rename create_entity_device_class_group to create_entity_groups

--- python_scripts/create_all_group.py
+++
@@ -1,5 +0,0 @@
-domain = data.get('domain')
-group = data.get('group')
-
-service_data = {"object_id": group, "entities": hass.states.entity_ids(domain)}
-hass.services.call("group", "set", service_data, False)
